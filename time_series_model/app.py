0aa38b4b70af3e219a603690d1858e4e7b8dc669
Hasta <hasta.suci@gmail.com>
2024-11-30 15:31:52 +0700

    add comment in app.py

--- time_series_model/app.py
+++ time_series_model/app.py
@@ -2,7 +2,6 @@ import flask
 from flask import Flask, request, jsonify
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import load_model
 import numpy as np
 
 # Load model
@@ -19,15 +18,15 @@ def predict():
 
     # Validate input
     if "calories" not in data or not isinstance(data['calories'], list):
-        return jsonify({'error': "Input harus berisi list dengan 7 angka."}), 400
+        return jsonify({'error': "Input must contain a list with 7 numbers."}), 400
     
     try:
-        # Pastikan input memiliki panjang 7
+        # Make sure input has length of 7
         input_data = np.array(data['calories'])
         if len(input_data) != 7:
-            return jsonify({'error': "Input harus berupa list dengan panjang 7."}), 400
+            return jsonify({'error': "Input must be a list of length 7."}), 400
 
-        # Reshape data menjadi (1, 7, 1) -> (batch_size, timesteps, features)
+        # Reshape data into (1, 7, 1) -> (batch_size, timesteps, features)
         input_data = input_data.reshape((1, 7, 1))
         
         # Predict
@@ -42,6 +41,6 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# Jalankan server Flask
+# Run Flask server
 if __name__ == '__main__':
     app.run(debug=True)
